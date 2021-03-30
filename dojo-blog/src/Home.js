207ff88af823c06bfadc0a227a1b1187ed604362
Marko <29685366+MarkoTON@users.noreply.github.com>
2021-03-30 17:00:01 +0200

    adding keyID in map when loops

--- dojo-blog/src/Home.js
+++ dojo-blog/src/Home.js
@@ -10,8 +10,17 @@ const Home = () => {
 
   return (
     <div className="home">
-      {blogs.map(blog => (
+      {blogs.map((blog) => (
         <div className="blog-preview" key={blog.id} >
+          <h2>{ blog.title }</h2>
+          <p>Written by { blog.author }</p>
+        </div>
+      ))}
+
+      <hr/>
+
+      {blogs.map((blog,keyID) => (
+        <div className="blog-preview" key={keyID} >
           <h2>{ blog.title }</h2>
           <p>Written by { blog.author }</p>
         </div>
