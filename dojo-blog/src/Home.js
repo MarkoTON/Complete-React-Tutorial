a32ab02a34fcbca4bafe0891025cbf9e772a7051
Marko <29685366+MarkoTON@users.noreply.github.com>
2021-03-31 10:52:34 +0200

    checking out useEffect

--- dojo-blog/src/Home.js
+++ dojo-blog/src/Home.js
@@ -18,12 +18,20 @@ const Home = () => {
   useEffect(() => {
     console.log('use effect ran');
     console.log(blogs);
+    console.log(name);
+    // console.log(useState);
+    // console.log(useState('name'));
+    console.log(setName);
+    console.log(setName());
+    console.log(setName(5));
+    // Bice dva puta da renderuje i u prvom je Mario u drugom je promenjen na 5
   }, [name])
 
   return (
     <div className="home">
       <BlogList blogs={blogs} title="All Blogs" handleDelete={handleDelete} />
       <button onClick={() => setName('luigi')}>change name</button>
+      <p>{name}</p>
     </div>
   );
 }
