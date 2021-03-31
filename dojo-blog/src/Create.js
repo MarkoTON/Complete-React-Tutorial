5da9a5f0b778dedc9dfeead82a489473afeee1d0
Marko <29685366+MarkoTON@users.noreply.github.com>
2021-03-31 15:53:46 +0200

    small fix with Create.js

--- dojo-blog/src/Create.js
+++ dojo-blog/src/Create.js
@@ -5,17 +5,22 @@ const Create = () => {
   const [title, setTitle] = useState('');
   const [body, setBody] = useState('');
   const [author, setAuthor] = useState('mario');
+  const [isPending, setPending] = useState(false);
   const history = useHistory();
 
   const handleSubmit = (e) => {
     e.preventDefault();
     const blog = { title, body, author };
 
+    setPending(true);
+
     fetch('http://localhost:8000/blogs/', {
       method: 'POST',
       headers: { "Content-Type": "application/json" },
       body: JSON.stringify(blog)
     }).then(() => {
+      console.log('new blog added');
+      setPending(false);
       // history.go(-1);
       history.push('/');
     })
@@ -46,7 +51,8 @@ const Create = () => {
           <option value="mario">mario</option>
           <option value="yoshi">yoshi</option>
         </select>
-        <button>Add Blog</button>
+        { !isPending && <button>Add Blog</button>}
+        { isPending && <button disabled>Add blog...</button>}
       </form>
     </div>
   );
