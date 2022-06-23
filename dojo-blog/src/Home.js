931e1c52e06e233edfaf3769bfcee05e2abd9493
Marko_TON <markotoncanovic@gmail.com>
2022-06-23 11:23:33 +0200

    checking useEffect

--- dojo-blog/src/Home.js
+++ dojo-blog/src/Home.js
@@ -17,14 +17,15 @@ const Home = () => {
 
   useEffect(() => {
     console.log('use effect ran');
-    console.log(blogs);
-    console.log(name);
+    // console.log(blogs);
+    // console.log(name);
     // console.log(useState);
     // console.log(useState('name'));
-    console.log(setName);
-    console.log(setName());
-    console.log(setName(5));
+    // console.log(setName);
+    // console.log(setName());
+    // console.log(setName(5));
     // Bice dva puta da renderuje i u prvom je Mario u drugom je promenjen na 5
+    // Ako ostavimo na kraju prazne zagrade [] onda ce useEffect biti pokrenuta samo jednom tokom rendiranja. To je drugi uslov kada se ova funkcija pokrece
   }, [name])
 
   return (
