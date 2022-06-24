642ba1651fb1db64a0c4325fdc91042b372d105e
Marko_TON <markotoncanovic@gmail.com>
2022-06-24 13:40:54 +0200

    adding css and testing Form in Create.js

--- dojo-blog/src/Create.js
+++ dojo-blog/src/Create.js
@@ -3,34 +3,43 @@ import { useState } from "react";
 const Create = () => {
   const [title, setTitle] = useState('');
   const [body, setBody] = useState('');
-  const [author, setAuthor] = useState('mario');
+  const [author, setAuthor] = useState('');
+
+  // add function onChange for select to throw arror if it's empty
 
   return (
     <div className="create">
       <h2>Add a New Blog</h2>
       <form>
-        <label>Blog title:</label>
+        <label>Blog title <span>*</span> :</label>
         <input 
           type="text" 
           required 
           value={title}
+          placeholder="Metallica news"
           onChange={(e) => setTitle(e.target.value)}
         />
-        <label>Blog body:</label>
+        <label>Blog body <span>*</span> :</label>
         <textarea
           required
           value={body}
+          placeholder="Metallica is in Belgrade again!..."
           onChange={(e) => setBody(e.target.value)}
         ></textarea>
-        <label>Blog author:</label>
+        <label>Blog author <span>*</span> :</label>
         <select
           value={author}
+          required
           onChange={(e) => setAuthor(e.target.value)}
         >
+          <option value="">Please select author</option>
           <option value="mario">mario</option>
           <option value="yoshi">yoshi</option>
         </select>
         <button>Add Blog</button>
+        <p>{title}</p>
+        <p>{body}</p>
+        <p>{author}</p>
       </form>
     </div>
   );
